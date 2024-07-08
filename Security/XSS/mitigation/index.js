d06dd9547bb22891580d51dc628dcf04d91a09c2
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2024-07-08 23:44:47 +0530

    graphql practice

--- Security/XSS/mitigation/index.js
+++ Security/XSS/mitigation/index.js
@@ -4,9 +4,17 @@ const PORT = 3010;
 const app = express();
 
 app.use((req, res, next) => {
+    // CSP provides different headers to control script loading
+    // Allowed sources
+    // eg. default-src img-src media-src font-src to which you can assign N no of values
+    // for inine css you can give "unsafe-inline"
+    // 
+    // Script Nonces
+    // To restrict user from only loading inline script with particular nonce/key
+    // eg.script-src 'nonce-randomKey' //only script with nonce='randomKey'
     res.setHeader(
         'Content-Security-Policy',
-        "default-src 'self';" + 
+        "default-src 'self';" +
         "script-src 'self' 'nonce-randomKey' 'unsafe-inline' http://unsecure.com;"
     );
     next();
